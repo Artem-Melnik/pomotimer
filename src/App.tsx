c379f53572a4cb39624786d3dacfd8695a0ee040
DesignHacker <66458748+Artem-Melnik@users.noreply.github.com>
2025-04-27 16:08:56 -0700

    Added formatting to timer text

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,86 @@
 import { useState } from 'react';
-// import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
 import './App.css';
+import * as React from "react";
+
+const timerSec: number = 105*60;
+
+function Counter({ count }: { count: number }) {
+  function pad(num: number) {
+    return String(num).padStart(2, "0");
+  }
+
+  let timerStr: string = pad(count%60);
+
+  if (timerSec >= 60) {
+    timerStr = pad(Math.floor(count/60)%60) + ":" + timerStr;
+  }
+  if (timerSec >= 60*60) {
+    timerStr = pad(Math.floor(count/(60*60))) + ":" + timerStr;
+  }
+  return (
+      <div>
+        <h1>{timerStr}</h1>
+      </div>
+  );
+}
+
+type Callback = {
+  call: () => void;
+};
+
+const MyButton: React.FC<Callback> = React.memo(({ call }) => {
+  const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
+
+  function start() {
+    const interval = setInterval(call, 1000);
+    setTimer(interval);
+    console.log('tick started', interval);
+  }
+
+  function stop() {
+    if (timer !== null) {
+      clearInterval(timer);
+      setTimer(null);
+      console.log('tick stopped');
+    }
+  }
+
+  console.log("my button");
+  return (
+      <button onClick={timer === null ? start : stop}>
+        {timer === null ? 'Start' : 'Stop'}
+      </button>
+  );
+});
 
 function App() {
-  let timer: NodeJS.Timeout;
+  const [count, setCount] = useState(timerSec);
 
-  function Counter() {
-    const [count, setCount] = useState(0);
+  const tick = React.useCallback(() => {
+    setCount((c) => c - 1);
+    console.log('tick');
+  }, []);
 
-    function tick() {
-      setCount((c) => c + 1);
-    }
-    return (
-      <div>
-        <h1>{count}</h1>;
-      </div>
-    );
-  }
-
-  function MyButton(tick: Function) {
-    function start() {
-      timer = setInterval(tick, 1000);
-      console.log('tick started', timer);
-    }
-
-    function stop() {
-      console.log('Stopping', timer);
-      clearInterval(timer);
-    }
-    if (timer == null) {
-      return <button onClick={start}>Start</button>;
-    } else {
-      return <button onClick={stop}>Stop</button>;
-    }
-  }
-
+  console.log(tick);
   return (
-    <div className="App">
-      <MyButton />
-      <Counter />
-      <header className="App-header">
-        <p>
-          This is a test Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Counter count={count} />
+        <MyButton call={tick} />
+        {/*<header className="App-header">*/}
+        {/*  <p>*/}
+        {/*    This is a test. Edit <code>src/App.tsx</code> and save to reload.*/}
+        {/*  </p>*/}
+        {/*  <a*/}
+        {/*      className="App-link"*/}
+        {/*      href="https://reactjs.org"*/}
+        {/*      target="_blank"*/}
+        {/*      rel="noopener noreferrer"*/}
+        {/*  >*/}
+        {/*    Learn React*/}
+        {/*  </a>*/}
+        {/*</header>*/}
+      </div>
   );
 }
 
 export default App;
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
